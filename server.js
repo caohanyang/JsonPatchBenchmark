156934e7870fdc1210017abbfa4965ed3bdbdf06
caohanyang <caohanyang007@gmail.com>
2015-04-14 11:03:55 +0200

    change port

--- server.js
+++ server.js
@@ -24,5 +24,4 @@ app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
-console.log('listen on 8080');
+app.listen(process.env.port || 5000);
